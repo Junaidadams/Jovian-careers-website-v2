c2375e2af9762751aefe342f2fc7a8f1945b9c28
Junaidadams <138676428+Junaidadams@users.noreply.github.com>
2023-07-25 13:54:18 +0000

    modified:   app.py 	modified:   database.py 	new file:   templates/banner.html 	new file:   templates/bootstrap.html 	modified:   templates/home.html 	modified:   templates/jobitem.html 	new file:   templates/jobpage.html

--- database.py
+++ database.py
@@ -15,3 +15,12 @@ def load_jobs_from_db():
             result_dicts.append(dict(row._mapping))
         return result_dicts
     
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM JOBS WHERE id=:val"),
+                              val = id)
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
